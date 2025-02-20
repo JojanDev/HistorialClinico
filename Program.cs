5d5aab7085c5cf84c0799221c90d2788c04b41ec
Johan Delgado <johan24.dev@gmail.com>
2025-02-20 11:44:08 -0500

    historial terminado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HistorialClinico
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HistorialClinico
 {
     class Mascota
     {
@@ -16,9 +18,8 @@
 
     class Historial
     {
-        public string Detalles;
-        public DateTime Fecha;
-        public Historial(string detalles, DateTime fecha)
+        public string Detalles, Fecha;
+        public Historial(string detalles, string fecha)
         {
             Detalles = detalles;
             Fecha = fecha;
@@ -32,42 +33,191 @@
 
         public void registrarMascota()
         {
-            Console.Write("Ingrese el nombre de la mascota: ");
+            Console.Write("\nIngrese el nombre de la mascota: ");
             string nombre = Console.ReadLine();
 
             Console.Write("Ingrese la raza de la mascota: ");
             string raza = Console.ReadLine();
 
-            Console.Write("Ingrese la edad");
+            Console.Write("Ingrese la edad: ");
             int edad = int.Parse(Console.ReadLine());
 
             Mascota mascota = new (nombre, raza, edad);
 
             listaMascotas.Add(mascota);
 
-            for (int i = 0; i < listaMascotas.Count; i++)
-            {
-                Console.WriteLine(listaMascotas[i]);
-                for (int i = 0; i < listaMascotas[i].historiales.Count; i++)
-                {
 
-                }
+            menu();
+        }
+
+
+
+        public void añadirAntecedente()
+        {
+            if (validarLista())
+            {
+                Console.Write("\nIngrese el indice de la mascota a añadir el antecendente: ");
+                int indice = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese la informacion de el antecendente: ");
+                string detalles = Console.ReadLine();
+
+                Console.Write("Ingrese la fecha de el antecendente (DD/MM/AAAA): ");
+                string fechaString = Console.ReadLine();
+
+                Historial historial = new Historial(detalles, fechaString);
+                listaMascotas[indice - 1].historiales.Add(historial);
+            }
+            else
+            {
+                Console.WriteLine("NO SE HA REGISTRADO NINGUNA MASCOTA, NO SE PUEDE AÑADIR UN HISTORIAL\n");
             }
         }
 
+        public void mostrarAntecedentes()
+        {
+            if (validarLista())
+            {
+                Console.Write("\nIngrese el indice de la mascota a mostrar el antecedente: ");
+                int indice = int.Parse(Console.ReadLine());
+
+                List<Historial> historial = listaMascotas[indice - 1].historiales;
+
+                if (historial.Count > 0)
+                {
+                    for (int i = 0; i < historial.Count; i++)
+                    {
+                        Console.WriteLine($"-HISTORIAL {i+1}. Fecha: {historial[i].Fecha} \n" +
+                            $"Descripcion: {historial[i].Detalles}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nLA MASCOTA NO TIENE HISTORIAL\n");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("NO SE HA REGISTRADO NINGUNA MASCOTA, NO HAY HISTORIAL PARA MOSTRAR\n");
+            }
+
+        }
+
+        public bool validarLista()
+        {
+            return listaMascotas.Count > 0;
+        }
+
+        public void mostrarMascotas()
+        {
+            Console.WriteLine("\n--------- LISTA DE MASCOTAS ---------\n");
+
+            if (validarLista())
+            {
+                for (int i = 0; i < listaMascotas.Count; i++)
+                {
+                    Console.WriteLine($"{i+1}. {listaMascotas[i].Nombre}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO SE HA REGISTRADO NINGUNA MASCOTA\n");
+            }
+        }
+
+        public void eliminarMascota()
+        {
+            if (validarLista())
+            {
+                while (true)
+                {
+                    Console.Write("Ingrese el indice de la mascota a eliminar: ");
+                    int indice = int.Parse(Console.ReadLine());
+
+                    if (indice > 0 && indice <= listaMascotas.Count)
+                    {
+                        listaMascotas.RemoveAt(indice - 1);
+                        Console.WriteLine("MASCOTA ELIMINADA CORRECTAMENTE");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("INDICE INEXSISTENTE");
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO SE HA REGISTRADO NINGUNA MASCOTA, NO SE PUEDE ELIMINAR UNA MASCOTA SI NO EXISTEN MASCOTAS\n");
+            }
+        }
+
+        //registrar, añadir, mostrarmascotas y antecedentes, eliminar mascota
+
         public void menu()
         {
-            Console.WriteLine("------------- HISTORIAL CLINICO -------------\n" +
+            Console.Write("\n" +
+                              "MENU: \n" +
                               "\n" +
-                              "1. Registrar mascota." +
-                              "2. Añadir antecedente." +
-                              "3. ");
+                              "1. Registrar mascota.\n" +
+                              "2. Añadir historial a una mascota.\n" +
+                              "3. Mostrar mascotas.\n" +
+                              "4. Mostrar historial de una mascota.\n" +
+                              "5. Eliminar mascota.\n"+
+                              "0. Salir.\n" +
+                              "\n" +
+                              "Ingrese la opcion: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    registrarMascota();
+                    menu();
+                    break;
+                case 2:
+                    añadirAntecedente();
+                    menu();
+                    break;
+                case 3:
+                    mostrarMascotas();
+                    menu();
+                    break;
+                case 4:
+                    mostrarAntecedentes();
+                    menu();
+                    break;
+                case 5:
+                    eliminarMascota();
+                    menu();
+                    break;
+                case 0:
+                    Console.WriteLine("FINALIZO EL PROGRAMA");
+                    break;
+                default:
+
+                    Console.WriteLine("\nERROR: INGRESO UNA OPCION INVALIDA!!!");
+                    menu();
+                    break;
+            }
         }
 
         static void Main(string[] args)
         {
+            Program pers1 = new Program();
             Console.WriteLine("------------- BIENVENIDOS A PIPO'S VETERINARY -------------");
-            string opcion = Console.ReadLine();
+            pers1.menu();
+            //Console.Write("Ingrese la fecha de el antecendente (DD/MM/AAAA): ");
+            //string fechaString = Console.ReadLine();
+            //DateTime fecha = DateTime.Parse(fechaString);
+            //string fecha2 = fecha.ToString("d");
+            //Console.WriteLine(fecha2);
+
+            //List<int> lista = new List<int>();
+            //Console.WriteLine(lista.Count);
         }
     }
 }
--- obj/Debug/net8.0/HistorialClinico.AssemblyInfo.cs
+++ obj/Debug/net8.0/HistorialClinico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HistorialClinico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ebbcc9de00381e2ac654933c39dacc9f82a7da")]
 [assembly: System.Reflection.AssemblyProductAttribute("HistorialClinico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HistorialClinico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
